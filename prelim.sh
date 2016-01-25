48bde0d5d5612cc9d4cdce451ea75d3f2d0e62f6
Yew Long Leong <yewlong42@gmail.com>
2016-01-26 00:32:55 +0800

    prelim now also logs to a file

--- prelim.sh
+++ prelim.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+LOGFILE=prelim.log
 echo "You entered $# arguments. The script is called $(basename $0)"
 
 if [ $# -gt 0 ]; then # $# is argc
@@ -9,8 +10,8 @@ if [ $# -gt 0 ]; then # $# is argc
    done
 fi
 
-echo "The date and time is $(date)\n"
-echo "The logged in users are: \n$(who)\n"
-echo "The uptime is : $(uptime)\n"
+echo "The date and time is $(date)\n" | tee -a $LOGFILE
+echo "The logged in users are: \n$(who)\n" | tee -a $LOGFILE
+echo "The uptime is : $(uptime)\n" | tee -a $LOGFILE
 
 exit 0
